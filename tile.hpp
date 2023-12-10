c97205bba9e516b025507be01bdad1463378bafb
ajangda4 <122679382+ajangda4@users.noreply.github.com>
2023-12-10 22:05:19 +0500

    Update tile.hpp

--- tile.hpp
+++ tile.hpp
@@ -3,9 +3,20 @@
 #include <SDL_image.h>
 #include "entity.hpp"
 #include "vector2f.hpp"
-
+#include "screen.hpp" 
 class Tile : public Entity {
 public: 
     Tile(Vector2f p_pos, SDL_Texture* p_tex);
+    Tile(){};
 
+};
+
+class Rectangle:public Entity{
+    private:
+    Tile tile1;
+    Tile tile2;
+    public:
+        Rectangle(Vector2f p_pos, Vector2f p_pos2 , SDL_Texture* p_tex);
+
+        void render(Screen& screen);
 };
