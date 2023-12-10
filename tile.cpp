34c9a875190b7255d822a85989f6b3088ccd3888
ajangda4 <122679382+ajangda4@users.noreply.github.com>
2023-12-10 22:04:57 +0500

    Update tile.cpp

--- tile.cpp
+++ tile.cpp
@@ -1,3 +1,21 @@
 #include "tile.hpp"
 
 Tile::Tile(Vector2f pos, SDL_Texture* tex) : Entity(pos, tex) {}
+
+
+Rectangle::Rectangle(Vector2f p_pos, Vector2f p_pos2, SDL_Texture* p_tex)
+    : tile1(p_pos, p_tex), tile2(p_pos2, p_tex)
+{
+    
+}
+
+void Rectangle::render(Screen& screen) {
+    screen.render(tile1);
+    screen.render(tile2);
+}
+
+
+
+
+
+
