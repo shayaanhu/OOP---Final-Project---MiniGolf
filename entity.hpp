d758914daf2db5747640dfc67faf8c8e5722aaa8
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2023-12-11 14:02:49 +0500

    Update entity.hpp

--- entity.hpp
+++ entity.hpp
@@ -6,9 +6,8 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
-
 class Entity {
-private:
+protected:
     Vector2f position;
     SDL_Texture* texture;
     SDL_Rect currentFrame;
@@ -16,22 +15,26 @@ private:
     float angle = 0;
 
 public:
+    // Constructors:
     Entity();
     Entity(Vector2f pos, SDL_Texture* tex);
 
+    // Member functions:
     void setPosition(float x, float y);
-    Vector2f getPosition();
     SDL_Rect getCurrentFrame();
     SDL_Texture* getTexture();
-    void clear();
     void setScale(float w, float h);
     Vector2f getScale();
     void setAngle(float ang);
     float getAngle();
 
+    // Polymorphism:
+    // Every entity has different dimensions, hence we need to override getPosition to account for that.
+    virtual Vector2f getPosition();
 
-    ~Entity(); 
+    // Virtual destructor:
+    virtual ~Entity(); 
 
 };
 
-#endif // ENTITY_HPP
+#endif
