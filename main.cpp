1498ae671664357e00cd428fedb59ead0a08e523
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2023-12-11 14:01:37 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "ball.hpp"
 #include "hole.hpp"
 #include "tile.hpp"
+#include "rectangle.hpp"
 #include "arrow.hpp"
 #include "powermeter.hpp"
 #include "entity.hpp"
@@ -19,6 +20,7 @@ double deltaTime = 0;
 SDL_Event event;
 bool gameRunning = true;
 int state = 0; //0 = title screen, 1 = game, 2 = end screen
+int level = 0;
 bool mouseDown = false;
 bool mousePressed = false;
 
@@ -41,7 +43,16 @@ SDL_Texture* tileLightTexture = myScreen.loadTexture("tileLight.png");
 std::vector<Tile> tiles = {};
 std::vector<Rectangle> rect = {};
 
+SDL_Texture* startScreenTexture = myScreen.loadTexture("start.png");
 SDL_Texture* backgroundTexture = myScreen.loadTexture("backTest.png");
+SDL_Texture* endScreenTexture = myScreen.loadTexture("end.png");
+
+// Fonts
+TTF_Font* font = TTF_OpenFont("font.ttf", 50);
+SDL_Color white = {255, 255, 255};
+SDL_Color black = {0, 0, 0};
+
+// Music:
 Mix_Music* backgroundMusic = nullptr;
 
 void loadLevel(int level) {
@@ -49,65 +60,56 @@ void loadLevel(int level) {
     ball.setScale(0.2, 0.2);
     hole.setScale(0.175, 0.175);
     arrow.setPosition(-100, -100);
-    powerMeter.push_back(Entity(Vector2f(0,0), powerMeterBG));
-    powerMeter.push_back(Entity(Vector2f(0,0), powerMeterFG));
 
     switch (level) {
         case 0:
-        tiles.push_back(Tile(Vector2f(350, 100), tileLightTexture));
-        tiles.push_back(Tile(Vector2f(230, 100), tileLightTexture));
-        tiles.push_back(Tile(Vector2f(350, 200), tileLightTexture));
-        tiles.push_back(Tile(Vector2f(230, 200), tileLightTexture));
-        hole.setPosition(350, 200);
-
-        tiles.push_back(Tile(Vector2f(550, 40), tileLightTexture));
-        tiles.push_back(Tile(Vector2f(570, 200), tileLightTexture));
-
-        
-
-        
+        ball.setWin(false);
+        tiles.push_back(Tile(Vector2f(125, 150), tileLightTexture));
+        tiles.push_back(Tile(Vector2f(125, 50), tileLightTexture));
+        tiles.push_back(Tile(Vector2f(325, 150), tileLightTexture));
+        tiles.push_back(Tile(Vector2f(325, 50), tileLightTexture));
+        hole.setPosition(330, 180);
         break;
 
-        // case 1:
-        tiles.push_back(Tile(Vector2f(550, 40), tileLightTexture));
-        tiles.push_back(Tile(Vector2f(570, 200), tileLightTexture));
-        tiles.push_back(Tile(Vector2f(600, 120), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(480, 100), Vector2f(480, 150), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(660, 100), Vector2f(660, 150), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(635, 40), Vector2f(685, 40), tileLightTexture));
-        hole.setPosition(600, 180);
-        
-
-        
+        case 1:
+        ball.setWin(false);
+        rect.push_back(Rectangle(Vector2f(600, -70), Vector2f(600, -20), tileLightTexture));
+        rect.push_back(Rectangle(Vector2f(600, 80), Vector2f(600, 130), tileLightTexture));
+        rect.push_back(Rectangle(Vector2f(600, 230), Vector2f(600, 280), tileLightTexture));
+        hole.setPosition(800, 200);
         break;
 
         case 2:
-        tiles.push_back(Tile(Vector2f(165, 375), tileLightTexture));
-        tiles.push_back(Tile(Vector2f(590, 375), tileLightTexture));
-        tiles.push_back(Tile(Vector2f(510, 390), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(100, 340), Vector2f(100, 390), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(35, 220), Vector2f(35, 270), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(120, 200), Vector2f(120, 250), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(300, 250), Vector2f(350, 250), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(420, 200), Vector2f(470, 200), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(535, 250), Vector2f(585, 250), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(180, 200), Vector2f(230, 200), tileLightTexture));
-        rect.push_back(Rectangle(Vector2f(650, 310), Vector2f(650, 360), tileLightTexture));
-        hole.setPosition(400, 470);
+        ball.setWin(false);
+        rect.push_back(Rectangle(Vector2f(0, -70), Vector2f(0, -20), tileLightTexture));
+        rect.push_back(Rectangle(Vector2f(0, 80), Vector2f(0, 130), tileLightTexture));
+        rect.push_back(Rectangle(Vector2f(0, 230), Vector2f(0, 280), tileLightTexture));
+        hole.setPosition(0, 200);
+        break;
+
+        case 3:
+        ball.setWin(false);
+        tiles.push_back(Tile(Vector2f(225, -25), tileLightTexture));
+        tiles.push_back(Tile(Vector2f(225, 200), tileLightTexture));
+        hole.setPosition(330, 180);
+        break;
+
+        case 4:
+        ball.setWin(false);
+        state = 2;
         break;
     }
 }
+
 void loadMusic() {
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-    std::cerr << "SDL_mixer initialization failed: " << Mix_GetError() << std::endl;
-    // cleanup();
-    // return -1;
-    }
+    std::cerr << "SDL_mixer initialization failed: " << Mix_GetError() << std::endl; }
 
-    backgroundMusic = Mix_LoadMUS("C:\\Users\\coolm\\Desktop\\Project\\Project\\back.mp3"); // Replace with the actual path to your music file
+    backgroundMusic = Mix_LoadMUS("music.mp3"); // Replace with the actual path to your music file
     if (!backgroundMusic) {
         std::cerr << "Failed to load background music: " << Mix_GetError() << std::endl;
     }
+
 }
 void cleanup() {
     if (backgroundMusic) {
@@ -117,6 +119,7 @@ void cleanup() {
     Mix_CloseAudio(); // Clean up SDL_mixer
     SDL_Quit(); // SDL_Quit should be here
 }
+
 void playMusic() {
     Mix_PlayMusic(backgroundMusic, -1); // -1 means loop indefinitely
 }
@@ -136,6 +139,7 @@ void update() {
                 if (event.button.button == SDL_BUTTON_LEFT) {
                     mouseDown = true;
                     mousePressed = true;
+                    if (state == 0) state = 1;
                 }   
                 break;
             case SDL_MOUSEBUTTONUP:
@@ -145,27 +149,48 @@ void update() {
                 break;
         }
     }
-
+    
     arrow.setPosition(-100, -100); // Setting the arrow's position outside the screen while not in use.
-    ball.update(deltaTime, mouseDown, mousePressed, hole, tiles, arrow);
+
+    if (state == 1) ball.update(deltaTime, mouseDown, mousePressed, hole, tiles, rect, arrow);
+
+    // When ball is in the hole, proceed to the next level.
+    // std::cout << ball.getScale().x << " " << ball.getScale().y;
+    if (ball.getWin() == true and ball.getScale().x < - 5 or ball.getScale().y < - 5) {
+        level++;
+        loadLevel(level);
+    }
+
 }
 
 void graphics() {
     myScreen.clear();
-    myScreen.render(0, 0, backgroundTexture);
-    myScreen.render(arrow);
-    myScreen.render(ball);
-    myScreen.render(hole);
 
-    for (Tile& t : tiles) {
-        myScreen.render(t);
+    if (state == 0) myScreen.render(0, 0, startScreenTexture);
+
+    else if (state == 2) {
+        myScreen.render(0, 0, endScreenTexture);
+        // myScreen.renderCenter(0, 3 - 32, "YOU COMPLETED THE COURSE!", font, black);
     }
 
-    for (Entity& e : ball.getPowerMeter()) {
-        myScreen.render(e);
-    }
+    else if (state == 1) { 
+        myScreen.render(0, 0, backgroundTexture);
+        myScreen.render(arrow);
+        myScreen.render(ball);
+        myScreen.render(hole);
 
-    // render powermeter
+        for (Tile& t : tiles) {
+            myScreen.render(t);
+        }
+
+        for (Rectangle& r : rect) {
+            r.render(myScreen);
+        }
+
+        for (Entity& e : ball.getPowerMeter()) {
+            myScreen.render(e);
+        }
+    }
 
     myScreen.display();
 
@@ -175,29 +200,21 @@ int main(int argc, char* argv[]) {
 
     // Locking the FPS to roughly 120 (saves GPU power).
 	const int targetFPS = 140;
-	const int delayTime = 1000 / targetFPS;
+    const int delayTime = 1000 / targetFPS;
 	loadLevel(0);
-	loadMusic();
+    loadMusic();
 	playMusic();
-    
-	while (gameRunning) {
-    		update();
-        	graphics();
+
+    while (gameRunning) {
+    	update();
+        graphics();
 		SDL_Delay(delayTime);
-    	}
-	cleanup();
+    }
+
+    cleanup();
     
     return 0;
 }
 
 
-// #include "game.hpp"
-
-// int main(int argc, char* argv[])
-// {
-//     Game game;
-//     game.run();
-
-//     return 0;
-// }
 
