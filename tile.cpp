eedf60273acac67fb342858ea2821d14b37ac535
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2023-12-11 14:03:28 +0500

    Update tile.cpp

--- tile.cpp
+++ tile.cpp
@@ -2,20 +2,6 @@
 
 Tile::Tile(Vector2f pos, SDL_Texture* tex) : Entity(pos, tex) {}
 
-
-Rectangle::Rectangle(Vector2f p_pos, Vector2f p_pos2, SDL_Texture* p_tex)
-    : tile1(p_pos, p_tex), tile2(p_pos2, p_tex)
-{
-    
+Vector2f Tile::getPosition() {
+    return Vector2f(position.x + 60, position.y + 60);
 }
-
-void Rectangle::render(Screen& screen) {
-    screen.render(tile1);
-    screen.render(tile2);
-}
-
-
-
-
-
-
