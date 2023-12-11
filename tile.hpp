8ecb697332a0346ff3ffe22316c9f100166cf986
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2023-12-11 14:03:45 +0500

    Update tile.hpp

--- tile.hpp
+++ tile.hpp
@@ -3,20 +3,11 @@
 #include <SDL_image.h>
 #include "entity.hpp"
 #include "vector2f.hpp"
-#include "screen.hpp" 
+
 class Tile : public Entity {
 public: 
     Tile(Vector2f p_pos, SDL_Texture* p_tex);
-    Tile(){};
 
-};
+    virtual Vector2f getPosition() override;
 
-class Rectangle:public Entity{
-    private:
-    Tile tile1;
-    Tile tile2;
-    public:
-        Rectangle(Vector2f p_pos, Vector2f p_pos2 , SDL_Texture* p_tex);
-
-        void render(Screen& screen);
 };
