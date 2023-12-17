62f48738317415d1672df572926062c79779fb8f
ajangda4 <122679382+ajangda4@users.noreply.github.com>
2023-12-17 19:07:37 +0500

    Update hole.cpp

--- hole.cpp
+++ hole.cpp
@@ -3,4 +3,9 @@
 #include "vector2f.hpp"
 #include <SDL.h>
 
+Hole::Hole() {}
+
 Hole::Hole(Vector2f pos, SDL_Texture* tex) : Entity(pos, tex) {}
+Vector2f Hole::getPosition() {
+    return Vector2f(position.x + 20, position.y + 20);
+}
