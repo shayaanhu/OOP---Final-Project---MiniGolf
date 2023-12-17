aa7812bb8e16d2fb6ed9faa79890a75dbaa4059e
MuhammadMoazSiddiqui08084 <122269949+MuhammadMoazSiddiqui08084@users.noreply.github.com>
2023-12-17 11:40:49 +0500

    Update screen.hpp

--- screen.hpp
+++ screen.hpp
@@ -15,9 +15,10 @@ private:
     SDL_Renderer* renderer;
 
 public:
+    //constructor and destructor for screen
     Screen(const char* p_title, int p_w, int p_h);
     ~Screen();
-
+    //functions to run for making the screen, loading it and once game is closed then clearing it as well
     SDL_Texture* loadTexture(const char* filePath);
     void render(int x, int y, SDL_Texture* texture);
     void render(Entity& entity);
