9b7283808742c6c4bcd5e6ac4c14f7ce71c70431
MuhammadMoazSiddiqui08084 <122269949+MuhammadMoazSiddiqui08084@users.noreply.github.com>
2023-12-17 11:37:41 +0500

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,13 +1,13 @@
 #include "rectangle.hpp"
-
+//constructor for the rectangle class
 Rectangle::Rectangle(Vector2f p_pos, Vector2f p_pos2, SDL_Texture* p_tex)
     : tile1(p_pos, p_tex), tile2(p_pos2, p_tex) {}
-
+//rendering the tiles on the screen
 void Rectangle::render(Screen& screen) {
     screen.render(tile1);
     screen.render(tile2);
 }
-
+//finding and storing the positions of the tiles in a vector to be shown on screen
 std::vector<Vector2f> Rectangle::getPositions() {
     std::vector<Vector2f> positions;
     positions.push_back(tile1.getPosition());
@@ -15,7 +15,7 @@ std::vector<Vector2f> Rectangle::getPositions() {
     
     return positions;
 }
-
+// store the tiles in a vector that are to be shown on screen 
 std::vector<Tile> Rectangle::getTiles() {
     std::vector<Tile> tiles;
     tiles.push_back(tile1);
