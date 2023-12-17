e634fe0c5645673bf35057f9216931dbf6360c91
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2023-12-17 23:33:58 +0500

    Update screen.hpp

--- screen.hpp
+++ screen.hpp
@@ -9,19 +9,22 @@
 #include <vector>
 #include "entity.hpp"
 
+// Our game Screen.
+// All of the SDL related work is done here.
+
 class Screen {
 private:
     SDL_Window* window;
     SDL_Renderer* renderer;
 
 public:
-    //constructor and destructor for screen
     Screen(const char* p_title, int p_w, int p_h);
     ~Screen();
-    //functions to run for making the screen, loading it and once game is closed then clearing it as well
+
     SDL_Texture* loadTexture(const char* filePath);
     void render(int x, int y, SDL_Texture* texture);
     void render(Entity& entity);
+    void renderCenter(float p_x, float p_y, const char* p_text, TTF_Font* font, SDL_Color textColor); // For Fonts
     void clear();
     void display();
     SDL_Renderer* getRenderer();
