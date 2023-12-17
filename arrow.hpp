dc3289403ef3504f9d1d2c79c1d53a4234bc7f11
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2023-12-17 22:23:46 +0500

    Update arrow.hpp

--- arrow.hpp
+++ arrow.hpp
@@ -4,6 +4,9 @@
 #include "entity.hpp"
 #include "vector2f.hpp"
 
+// This is the arrow that points in the direction that the ball will be launched.
+// Inherited from Entity.
+
 class Arrow : public Entity {
 public: 
     Arrow();
