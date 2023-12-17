8c0047ea4251d6f0ad51cb87633078d71b2294ff
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2023-12-17 22:56:08 +0500

    Update powermeter.cpp

--- powermeter.cpp
+++ powermeter.cpp
@@ -3,4 +3,9 @@
 #include "vector2f.hpp"
 #include <SDL.h>
 
-Powermeter::Powermeter(Vector2f pos, SDL_Texture* tex) : Entity(pos, tex) {}
+Powermeter::Powermeter(Vector2f pos, SDL_Texture* fgTex, SDL_Texture* bgTex) : Entity(pos, fgTex), backgroundTex(bgTex) {} 
+
+void Powermeter::setTexture(SDL_Texture* fgTex, SDL_Texture* bgTex) {
+    foregroundTex = fgTex;
+    backgroundTex = bgTex;
+}
