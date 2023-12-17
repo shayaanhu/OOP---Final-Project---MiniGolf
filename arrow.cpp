9a4e55a75dc2d4050f3e471907adfb1369bc528f
ajangda4 <122679382+ajangda4@users.noreply.github.com>
2023-12-17 19:03:02 +0500

    Update arrow.cpp

--- arrow.cpp
+++ arrow.cpp
@@ -1,6 +1,9 @@
+
 #include "arrow.hpp"
 #include "entity.hpp"
 #include "vector2f.hpp"
 #include <SDL.h>
 
-Arrow::Arrow(Vector2f pos, SDL_Texture* tex) : Entity(pos, tex) {}
+Arrow::Arrow() {}
+
+Arrow::Arrow(Vector2f pos, SDL_Texture* tex) : Entity(pos, tex) {
