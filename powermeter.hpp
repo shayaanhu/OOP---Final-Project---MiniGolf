a98a39702f14137fb5efa30daf49bf0d7b819a75
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2023-12-19 20:08:48 +0500

    Update powermeter.hpp

--- powermeter.hpp
+++ powermeter.hpp
@@ -4,17 +4,14 @@
 #include "entity.hpp"
 #include "vector2f.hpp"
 
-// The powerMeter for the ball. 
-// Indicates how much power the ball is launched with.
-// Inherited from Entity.
 class Powermeter : public Entity {
 private:
     SDL_Texture* foregroundTex;
     SDL_Texture* backgroundTex;
 
 public: 
+    Powermeter(Vector2f pos, SDL_Texture* tex);
     Powermeter(Vector2f pos, SDL_Texture* fgTex, SDL_Texture* bgTex);
-
     void setTexture(SDL_Texture* fgTex, SDL_Texture* bgTex);
 
 };
